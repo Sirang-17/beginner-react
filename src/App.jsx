d7134e111f8e4910842ae1072c01fbe54a823727
Sirang-17 <sirangmuhammad17@gmail.com>
2026-04-24 03:11:09 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello React</h1>
+      <h1>Welcome to React!</h1>
+      <h1>2 + 5</h1>
+      <h1>{2 + 5}</h1> // JSX means JavaScript XML, so we can write JavaScript 
+      code inside the curly  braces
+      <h1>{2 - 5}</h1>
+    </div>
+  )
+}
+
+export default App
+
